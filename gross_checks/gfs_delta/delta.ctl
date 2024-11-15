c59d3a3b83765fb11c9f98fc262889cb9811a435
Robert Grumbine <Robert.Grumbine@noaa.gov>
2024-11-15 16:42:00 +0000

    continue on pgrb delta analy

--- gross_checks/gfs_delta/delta.ctl
+++ gross_checks/gfs_delta/delta.ctl
@@ -1,7 +1,6 @@
-# name min max  maxmin minmax
 o3mr    -0.4e-5   0.4e-5   -0.2e-5   0.2e-5
 absv    -3.0e-3   3.0e-3   -3.0e-4   3.0e-4     
-q       -1.5e-3   1.5e-3   -1.5e-4   1.5e-4 
+q       -7.5e-3   7.5e-3   -1.5e-4   1.5e-4 
 u      -75.      75.      -25.      25.  
 v      -75.      75.      -25.      25. 
 wz      -8.       8.       -1.       1. 
@@ -13,5 +12,3 @@ grle    -1.0e-2   1.0e-2   -1.0e-3   1.0e-3
 snmr    -2.0e-3   2.0e-3   -2.0e-4   2.0e-4 
 icmr    -1.5e-3   1.5e-3   -1.5e-4   1.5e-4 
 clwmr   -1.5e-3   1.5e-3   -1.5e-4   1.5e-4 
-
-# global bounds on differences
--- gross_checks/gfs_delta/pgrb.runup.sh
+++ gross_checks/gfs_delta/pgrb.runup.sh
@@ -1,44 +1,56 @@
 #!/bin/sh
 #
 
+source $HOME/env3.10/bin/activate
+export PYTHONPATH=$PYTHONPATH:$HOME/rgdev/ice_scoring/gross_checks/shared/
+
 #for cyc in 00 06 12 18
 #do
 cyc=00
 
 #for dd in 19 20 21 22 23
-#do
-tag=20240219
-
-ctrla=$HOME/noscrub/conctest/ctrl${cyc}/gfs.${tag}/${cyc}/atmos
-expta=$HOME/noscrub/conctest/test${cyc}/gfs.${tag}/${cyc}/atmos
-
-ctrlb=$HOME/noscrub/conctest/pgrb/ctrl${cyc}/gfs.${tag}/${cyc}/atmos
-exptb=$HOME/noscrub/conctest/pgrb/test${cyc}/gfs.${tag}/${cyc}/atmos
-
-
-#set -x
-export hhh=000
-delta=1
-while [ $hhh -le 384 ]
-#while [ $hhh -le 24 ]
+for dd in 20 21 22 23
 do
-  #A:
-  fna=gfs.t${cyc}z.pgrb2.0p25.f${hhh}
-  #B:
-  fnb=gfs.t${cyc}z.pgrb2b.0p25.f${hhh}
+  export tag=202402$dd
 
-  python3 grib_bootstrap.py $ctrla/$fna $expta/$fna > a.${hhh}.$tag 
-  python3 grib_bootstrap.py $ctrlb/$fnb $exptb/$fnb > b.${hhh}.$tag 
+  ctrla=$HOME/noscrub/conctest/ctrl${cyc}/gfs.${tag}/${cyc}/atmos
+  expta=$HOME/noscrub/conctest/test${cyc}/gfs.${tag}/${cyc}/atmos
 
-  if [ $hhh -ge 120 ] ; then
-    delta=3
+  ctrlb=$HOME/noscrub/conctest/pgrb/ctrl${cyc}/gfs.${tag}/${cyc}/atmos
+  exptb=$HOME/noscrub/conctest/pgrb/test${cyc}/gfs.${tag}/${cyc}/atmos
+
+  if [ ! -d ${tag}$cyc ] ; then
+    mkdir ${tag}$cyc
   fi
-  hhh=`expr $hhh + $delta`
 
-  if [ $hhh -lt 10 ] ; then
-    hhh=00$hhh
-  elif [ $hhh -lt 100 ] ; then
-    hhh=0$hhh
-  fi
+  # Loop over all leads:
+  #set -x
+  export hhh=000
+  delta=1
+  while [ $hhh -le 384 ]
+  #while [ $hhh -le 24 ]
+  do
+    #A:
+    fna=gfs.t${cyc}z.pgrb2.0p25.f${hhh}
+    #B:
+    fnb=gfs.t${cyc}z.pgrb2b.0p25.f${hhh}
+  
+    python3 grib_bootstrap.py $ctrla/$fna $expta/$fna > a.${hhh}.$tag 
+    python3 grib_bootstrap.py $ctrlb/$fnb $exptb/$fnb > b.${hhh}.$tag 
+  
+    mv [ab].${hhh}.$tag ${tag}$cyc
+  
+    if [ $hhh -ge 120 ] ; then
+      delta=3
+    fi
+    hhh=`expr $hhh + $delta`
+  
+    if [ $hhh -lt 10 ] ; then
+      hhh=00$hhh
+    elif [ $hhh -lt 100 ] ; then
+      hhh=0$hhh
+    fi
+
+  done
 
 done
